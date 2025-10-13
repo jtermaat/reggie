dea5bc81c57d6fabbc463542dfc42cc7deafac6f
johntermaat <john.termaat@gmail.com>
2025-10-13 16:48:08 -0500

    Adding enums for doctor specialization and licensed professional type.  Let's try tagging on these dimensions as well so that we can provide even more unique statistical queries during discussion.

--- reggie/models/comment.py
+++ reggie/models/comment.py
@@ -51,6 +51,181 @@ class Category(str, Enum):
     ANONYMOUS_NOT_SPECIFIED = "Anonymous / Not Specified"
 
 
+class DoctorSpecialization(str, Enum):
+    """Medical doctor (MD/DO) specializations based on ABMS board certifications."""
+    # Primary Care & General Specialties
+    FAMILY_MEDICINE = "family_medicine"
+    INTERNAL_MEDICINE = "internal_medicine"
+    PEDIATRICS = "pediatrics"
+    GENERAL_PRACTICE = "general_practice"
+
+    # Surgical Specialties
+    GENERAL_SURGERY = "general_surgery"
+    CARDIOTHORACIC_SURGERY = "cardiothoracic_surgery"
+    COLON_RECTAL_SURGERY = "colon_rectal_surgery"
+    NEUROLOGICAL_SURGERY = "neurological_surgery"
+    ORTHOPEDIC_SURGERY = "orthopedic_surgery"
+    OTOLARYNGOLOGY = "otolaryngology"  # ENT
+    PLASTIC_SURGERY = "plastic_surgery"
+    UROLOGY = "urology"
+    VASCULAR_SURGERY = "vascular_surgery"
+
+    # Medical Specialties
+    ALLERGY_IMMUNOLOGY = "allergy_immunology"
+    ANESTHESIOLOGY = "anesthesiology"
+    CARDIOLOGY = "cardiology"
+    CRITICAL_CARE_MEDICINE = "critical_care_medicine"
+    DERMATOLOGY = "dermatology"
+    EMERGENCY_MEDICINE = "emergency_medicine"
+    ENDOCRINOLOGY = "endocrinology"
+    GASTROENTEROLOGY = "gastroenterology"
+    GERIATRICS = "geriatrics"
+    HEMATOLOGY = "hematology"
+    INFECTIOUS_DISEASE = "infectious_disease"
+    NEPHROLOGY = "nephrology"
+    ONCOLOGY = "oncology"
+    PULMONOLOGY = "pulmonology"
+    RHEUMATOLOGY = "rheumatology"
+    SLEEP_MEDICINE = "sleep_medicine"
+    SPORTS_MEDICINE = "sports_medicine"
+
+    # Hospital-Based Specialties
+    HOSPITALIST = "hospitalist"
+    INTENSIVIST = "intensivist"
+    PATHOLOGY = "pathology"
+    RADIOLOGY = "radiology"
+    NUCLEAR_MEDICINE = "nuclear_medicine"
+
+    # Specialized Medical Fields
+    OBSTETRICS_GYNECOLOGY = "obstetrics_gynecology"
+    OPHTHALMOLOGY = "ophthalmology"
+    PAIN_MEDICINE = "pain_medicine"
+    PHYSICAL_MEDICINE_REHABILITATION = "physical_medicine_rehabilitation"
+    PREVENTIVE_MEDICINE = "preventive_medicine"
+    PSYCHIATRY = "psychiatry"
+
+    # Radiation & Imaging
+    RADIATION_ONCOLOGY = "radiation_oncology"
+    INTERVENTIONAL_RADIOLOGY = "interventional_radiology"
+
+    # Subspecialties Commonly Seen
+    RETINA_SPECIALIST = "retina_specialist"  # Ophthalmology subspecialty
+    INTERVENTIONAL_CARDIOLOGY = "interventional_cardiology"
+    ELECTROPHYSIOLOGY = "electrophysiology"
+    MATERNAL_FETAL_MEDICINE = "maternal_fetal_medicine"
+    NEONATOLOGY = "neonatology"
+    PEDIATRIC_SUBSPECIALTY = "pediatric_subspecialty"  # Various pediatric subspecialties
+
+    # Other
+    OSTEOPATHIC_MEDICINE = "osteopathic_medicine"  # DO with no specific specialty
+    UNSPECIFIED = "unspecified"
+    OTHER = "other"
+
+
+class LicensedProfessionalType(str, Enum):
+    """Non-physician licensed healthcare professional types."""
+    # Advanced Practice Nursing
+    NURSE_PRACTITIONER = "nurse_practitioner"  # NP, FNP, etc.
+    CERTIFIED_NURSE_ANESTHETIST = "certified_nurse_anesthetist"  # CRNA
+    CERTIFIED_NURSE_MIDWIFE = "certified_nurse_midwife"  # CNM
+    CLINICAL_NURSE_SPECIALIST = "clinical_nurse_specialist"  # CNS
+
+    # Nursing
+    REGISTERED_NURSE = "registered_nurse"  # RN
+    LICENSED_PRACTICAL_NURSE = "licensed_practical_nurse"  # LPN/LVN
+    CERTIFIED_NURSING_ASSISTANT = "certified_nursing_assistant"  # CNA
+
+    # Physician Assistant
+    PHYSICIAN_ASSISTANT = "physician_assistant"  # PA
+
+    # Therapy Professions
+    PHYSICAL_THERAPIST = "physical_therapist"  # PT
+    PHYSICAL_THERAPIST_ASSISTANT = "physical_therapist_assistant"  # PTA
+    OCCUPATIONAL_THERAPIST = "occupational_therapist"  # OT
+    OCCUPATIONAL_THERAPY_ASSISTANT = "occupational_therapy_assistant"  # OTA
+    SPEECH_LANGUAGE_PATHOLOGIST = "speech_language_pathologist"  # SLP
+    RESPIRATORY_THERAPIST = "respiratory_therapist"  # RT
+    RECREATIONAL_THERAPIST = "recreational_therapist"
+    MASSAGE_THERAPIST = "massage_therapist"  # LMT
+
+    # Optometry & Vision Care
+    OPTOMETRIST = "optometrist"  # OD
+    OPHTHALMIC_TECHNICIAN = "ophthalmic_technician"
+    OPHTHALMIC_ASSISTANT = "ophthalmic_assistant"
+    CERTIFIED_OPHTHALMIC_ASSISTANT = "certified_ophthalmic_assistant"  # COA
+    OPTOMETRIC_TECHNICIAN = "optometric_technician"
+
+    # Pharmacy
+    PHARMACIST = "pharmacist"  # PharmD, RPh
+    PHARMACY_TECHNICIAN = "pharmacy_technician"
+
+    # Dental Professions
+    DENTIST = "dentist"  # DDS/DMD
+    DENTAL_HYGIENIST = "dental_hygienist"
+    DENTAL_ASSISTANT = "dental_assistant"
+
+    # Podiatry
+    PODIATRIST = "podiatrist"  # DPM
+
+    # Chiropractic
+    CHIROPRACTOR = "chiropractor"  # DC
+
+    # Medical Laboratory & Diagnostic
+    MEDICAL_LABORATORY_SCIENTIST = "medical_laboratory_scientist"  # MLS
+    MEDICAL_LABORATORY_TECHNICIAN = "medical_laboratory_technician"  # MLT
+    PHLEBOTOMIST = "phlebotomist"
+    CYTOTECHNOLOGIST = "cytotechnologist"
+    HISTOTECHNOLOGIST = "histotechnologist"
+
+    # Radiology & Imaging Technologists
+    RADIOLOGIC_TECHNOLOGIST = "radiologic_technologist"
+    MRI_TECHNOLOGIST = "mri_technologist"
+    CT_TECHNOLOGIST = "ct_technologist"
+    ULTRASOUND_TECHNOLOGIST = "ultrasound_technologist"  # Sonographer
+    NUCLEAR_MEDICINE_TECHNOLOGIST = "nuclear_medicine_technologist"
+    RADIATION_THERAPIST = "radiation_therapist"
+    CARDIOVASCULAR_TECHNOLOGIST = "cardiovascular_technologist"
+
+    # Allied Health Professionals
+    DIETITIAN = "dietitian"  # RD, RDN
+    NUTRITIONIST = "nutritionist"
+    SOCIAL_WORKER = "social_worker"  # LCSW, LSW
+    CASE_MANAGER = "case_manager"
+    GENETIC_COUNSELOR = "genetic_counselor"
+    AUDIOLOGIST = "audiologist"
+
+    # Emergency & Surgical Support
+    PARAMEDIC = "paramedic"  # EMT-P
+    EMERGENCY_MEDICAL_TECHNICIAN = "emergency_medical_technician"  # EMT
+    SURGICAL_TECHNOLOGIST = "surgical_technologist"  # CST
+    SURGICAL_ASSISTANT = "surgical_assistant"
+
+    # Medical Assistants & Support Staff
+    MEDICAL_ASSISTANT = "medical_assistant"  # CMA, RMA
+    MEDICAL_SCRIBE = "medical_scribe"
+    PATIENT_CARE_TECHNICIAN = "patient_care_technician"
+
+    # Mental Health Professionals
+    LICENSED_PROFESSIONAL_COUNSELOR = "licensed_professional_counselor"  # LPC
+    LICENSED_CLINICAL_SOCIAL_WORKER = "licensed_clinical_social_worker"  # LCSW
+    MARRIAGE_FAMILY_THERAPIST = "marriage_family_therapist"  # LMFT
+    LICENSED_MENTAL_HEALTH_COUNSELOR = "licensed_mental_health_counselor"  # LMHC
+    PSYCHOLOGIST = "psychologist"  # PhD, PsyD
+    PSYCHIATRIC_TECHNICIAN = "psychiatric_technician"
+    BEHAVIOR_ANALYST = "behavior_analyst"  # BCBA
+
+    # Specialized Technicians & Coordinators
+    EKG_TECHNICIAN = "ekg_technician"
+    SLEEP_TECHNOLOGIST = "sleep_technologist"
+    PERFUSIONIST = "perfusionist"
+    ORTHOTIST_PROSTHETIST = "orthotist_prosthetist"
+    CLINICAL_RESEARCH_COORDINATOR = "clinical_research_coordinator"
+
+    # Other
+    UNSPECIFIED = "unspecified"
+    OTHER = "other"
+
+
 class Comment(BaseModel):
     """Comment model."""
 
